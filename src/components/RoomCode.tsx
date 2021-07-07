790b6a27eb067dd24fb1ba213180dac0451e208f
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2021-07-06 22:06:53 -0300

    started room code, css is not ready

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,14 @@
+import copyImg from '../images/copy.svg'
+
+import "../styles/room-code.scss"
+
+export function RoomCode() {
+  return(
+    <button className="room-code">
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #123456</span>
+    </button>
+  )
+}
